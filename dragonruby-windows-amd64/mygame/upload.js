56040656c9fbb475e2421a97ee3ddfa5713efafd
Paul Peragino <157921669+peraginop@users.noreply.github.com>
2024-05-14 15:35:51 -0400

    Delete dragonruby-windows-amd64/mygame/upload.js

--- dragonruby-windows-amd64/mygame/upload.js
+++
@@ -1,8 +0,0 @@
-var neocities = require('neocities')
-var api = new neocities('paulsprojects', 'Comp296!')
-
-api.upload([
-  {name: 'scoreData.json', path: './mygame/scoreData.json'}
-], function(resp) {
-  console.log(resp)
-})
